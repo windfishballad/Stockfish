12276771b6fe280c6105acca518219ac0f1eb535
windfishballad <windfishballadtcec@gmail.com>
2023-07-24 23:08:11 -0400

    Tweak replacement strategy (lower bound > upper bound)

--- src/tt.cpp
+++ src/tt.cpp
@@ -40,7 +40,7 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
       move16 = (uint16_t)m;
 
   // Overwrite less valuable entries (cheapest checks first)
-  if (   b == BOUND_EXACT
+  if (   (int) b >= (int) (genBound8 & 0x4)
       || (uint16_t)k != key16
       || d - DEPTH_OFFSET + 2 * pv > depth8 - 4)
   {
